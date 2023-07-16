7f28e1dd8eba52362b0e828741f19427844928f8
azufb <curiousazunas@gmail.com>
2023-07-16 21:30:02 +0900

    パス修正

--- index.ts
+++ index.ts
@@ -12,51 +12,17 @@ const port: number = 8000;
 
 // 型定義（スキーマ定義）
 const typeDefs = `
-  type Todo {
-    id: Int
-    title: String
-  }
-
-  type User {
-    id: Int
-    email: String
-    name: String
-    password: String
-    tasks: [Task]
-  }
-
   type Task {
     id: Int
     title: String
     deadLine: String
-    user: User
-    userId: Int
   }
 
   type Query {
     hello: String
     goodbye: String
-    todos: [Todo]
-    users: [User]
-  }
-
-  type Mutation {
-    addTodo(todo: String): [Todo]
-    addUser(name: String, email: String, password: String): User
-    deleteUser(email: String): User
   }
 `;
-
-const todos = [
-  {
-    id: 1,
-    title: '朝ごはんを食べる',
-  },
-  {
-    id: 2,
-    title: '昼ごはんを食べる',
-  },
-];
 
 // GraphQLにどういう処理をするか指示する
 const resolvers = {
@@ -65,36 +31,6 @@ const resolvers = {
     hello: () => 'こんにちは、あるいはこんばんは、エージェント黄昏くん。',
     // 「バイバイ！」と返してもらう
     goodbye: () => 'バイバイ！',
-    // 定義したtodosを取得する
-    todos: () => todos,
-    users: () => prisma.user.findMany(),
-  },
-  Mutation: {
-    // todosにデータを追加
-    addTodo: (parent: any, args: any) => {
-      const newData = {
-        id: 0,
-        title: args.todo,
-      };
-      todos.push(newData);
-      return todos;
-    },
-    addUser: (parent: any, args: any) => {
-      return prisma.user.create({
-        data: {
-          name: args.name,
-          email: args.email,
-          password: args.password,
-        },
-      });
-    },
-    deleteUser: (parent: any, args: any) => {
-      return prisma.user.delete({
-        where: {
-          email: args.email,
-        },
-      });
-    },
   },
 };
 
@@ -109,7 +45,7 @@ await server.start();
 
 // Expressのミドルウェア設定
 app.use(
-  '/',
+  '/api',
   cors<cors.CorsRequest>(),
   bodyParser.json(),
   expressMiddleware(server)
